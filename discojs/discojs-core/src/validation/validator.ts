8c7705d1220be6e185573aa9e083f7143ba98b22
s314cy <hugo.elguedj@epfl.ch>
2022-11-18 13:32:27 +0100

    discojs/validation: provide confusion matrix

--- discojs/discojs-core/src/validation/validator.ts
+++ discojs/discojs-core/src/validation/validator.ts
@@ -5,6 +5,11 @@ import { tf, data, Task, Logger, Client, GraphInformant, Memory, ModelSource } f
 export class Validator {
   private readonly graphInformant = new GraphInformant()
   private size = 0
+  private hits = 0
+  private classes = 1
+  private preds = List<number>()
+  private labels = List<number>()
+  private _confusionMatrix: number[][] | undefined
 
   constructor (
     public readonly task: Task,
@@ -18,41 +23,69 @@ export class Validator {
     }
   }
 
-  async assess (data: data.Data): Promise<void> {
+  async assess (data: data.Data, useConfusionMatrix?: boolean): Promise<void> {
     const batchSize = this.task.trainingInformation?.batchSize
     if (batchSize === undefined) {
       throw new TypeError('batch size is undefined')
     }
 
-    const labels: string[] | undefined = this.task.trainingInformation?.LABEL_LIST
-    const classes = labels?.length ?? 1
-
     const model = await this.getModel()
 
-    let hits = 0
+    this.hits = 0
+    this.preds = List()
+    this.labels = List()
+    this.classes = this.task.trainingInformation.LABEL_LIST?.length ?? 1
+
     await data.dataset.batch(batchSize).forEachAsync((e) => {
       if (typeof e === 'object' && 'xs' in e && 'ys' in e) {
         const xs = e.xs as tf.Tensor
-        const ys = (e.ys as tf.Tensor).dataSync()
+        const ys = e.ys as tf.Tensor
 
-        const pred = (model.predict(xs, { batchSize: batchSize }) as tf.Tensor)
-          .dataSync()
-          .map(Math.round)
+        const oneHotPreds = (model.predict(xs, { batchSize: batchSize }) as tf.Tensor)
 
         this.size += xs.shape[0]
 
-        hits += List(pred).zip(List(ys))
-          .map(([p, y]) => 1 - Math.abs(p - y))
-          .reduce((acc: number, e) => acc + e) / classes
+        // Get labels from one hot encoding
+        const preds = List(oneHotPreds.reshape([-1, this.classes]).arraySync() as number[][])
+          .map((p) => List(p)
+            .map((b, i) => Math.round(b) === 1 ? i : 0)
+            .reduce((acc: number, e) => acc + e))
+        const labels = List(ys.reshape([-1, this.classes]).arraySync() as number[][])
+          .map((p) => List(p)
+            .map((b, i) => b === 1 ? i : 0)
+            .reduce((acc: number, e) => acc + e))
 
-        const currentAccuracy = hits / this.size
+        // Keep track of prediction results for the confusion matrix
+        if (useConfusionMatrix) {
+          this.preds = this.preds.push(...preds)
+          this.labels = this.labels.push(...labels)
+        }
+
+        this.hits += preds.zip(labels)
+          .map(([p, l]) => p === l ? 1 : 0).reduce((acc: number, e) => acc + e)
+
+        const currentAccuracy = this.hits / this.size
         this.graphInformant.updateAccuracy(currentAccuracy)
       } else {
-        throw new TypeError('missing feature/label in dataset')
+        throw new Error('missing feature/label in dataset')
       }
     })
-    this.logger.success(`Obtained validation accuracy of ${this.accuracy()}`)
-    this.logger.success(`Visited ${this.visitedSamples()} samples`)
+
+    if (useConfusionMatrix) {
+      try {
+        this._confusionMatrix = tf.math.confusionMatrix(
+          this.labels.toArray(),
+          this.preds.toArray(),
+          this.classes
+        ).arraySync()
+      } catch (e: any) {
+        console.error(e instanceof Error ? e.message : e.toString())
+        throw new Error('Failed to compute the confusion matrix')
+      }
+    }
+
+    this.logger.success(`Obtained validation accuracy of ${this.accuracy}`)
+    this.logger.success(`Visited ${this.visitedSamples} samples`)
   }
 
   async getModel (): Promise<tf.LayersModel> {
@@ -67,15 +100,19 @@ export class Validator {
     throw new Error('cannot identify model')
   }
 
-  accuracyData (): List<number> {
+  get accuracyData (): List<number> {
     return this.graphInformant.data()
   }
 
-  accuracy (): number {
+  get accuracy (): number {
     return this.graphInformant.accuracy()
   }
 
-  visitedSamples (): number {
+  get visitedSamples (): number {
     return this.size
+  }
+
+  get confusionMatrix (): number[][] | undefined {
+    return this._confusionMatrix
   }
 }
